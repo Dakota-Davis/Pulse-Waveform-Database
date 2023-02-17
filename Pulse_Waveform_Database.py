9df866a8927fb13cbfc1260e1c00a974b531e403
Joshua Wood <joshua.randall.wood@gmail.com>
2023-02-17 12:19:08 -0600

    removed hidden tab characters

--- Pulse_Waveform_Database.py
+++ Pulse_Waveform_Database.py
@@ -18,29 +18,6 @@ parser.add_argument('-w', '--number_of_waveforms', type=int, nargs=1, help='The 
 
 args = parser.parse_args()
 
-""" #Previous method of making sure pmt & scintillator was specified#
-if args.scintillator is None:
-	parser.error('No scintillators specified. . . Please specify the type of scintillator(s) used')
-if args.scintillator is not None:
-	if len(args.scintillator) != 1:
-		parser.error('Incorrect number of scintillators specified. . . Please specify 1 scintillator')
-
-if args.pmt is None:
-	parser.error('No PMT specified. . . Please specify the type of PMT used')
-"""
-"""  #Will not require these to be specified for --add purposes#
-if args.element is None:
-	parser.error('No source specified. . . Please specify the gamma-ray source used')
-if args.element is not None and len(args.element) != 1:
-	parser.error('Too many sources specified. . . Please specify a single source')
-
-
-if args.run_number is None:
-	parser.error('No run specified. . . Please specify the collection run used')
-if args.element is not None and len(args.element) != 1:
-	parser.error('Too many runs specified. . . Please specify a single collection run')
-"""
-
 if args.add is not None:
     dir = "raw/%s/%s/" %(args.pmt[0], args.scintillator[0])  #directory name
     print(dir)
@@ -63,7 +40,6 @@ if args.psd_cut is not None:        #temporary check to prevent running errors w
     dir = "raw/%s/%s/" %(args.pmt[0], args.scintillator[0])
     path = os.path.join(dir, args.source[0] + ".csv")
     
-    #path = args.add			#Accepts input file
 
     out_dir = "data/%s/%s/" %(args.pmt[0], args.scintillator[0])  #output directory name
     print(out_dir)
@@ -72,24 +48,24 @@ if args.psd_cut is not None:        #temporary check to prevent running errors w
     os.system("mkdir -p " + out_dir)            #makes the output directory
     
     
-    f = open(path)				#Opens file
-    header = f.readline()			
+    f = open(path) #Opens file
+    header = f.readline()
     
-    lines = f.readlines()			#Reads file
+    lines = f.readlines() #Reads file
     #print(lines)
 
 
-    samps = 0			#Samples gathered
-    numosamps = 0			#Number of Samples wanted
-    if args.number_of_waveforms is None:			#Automates samples gathered for saving later on
+    samps = 0 #Samples gathered
+    numosamps = 0 #Number of Samples wanted
+    if args.number_of_waveforms is None: #Automates samples gathered for saving later on
         numosamps = len(lines)
     if args.number_of_waveforms is not None:
         if args.number_of_waveforms[0] > len(lines) or args.number_of_waveforms[0] < 1:
-            parser.error('Specified Samples out of Range. . . Enter a number between 1 and the file length [{}]'.format(len(lines)))	
+            parser.error('Specified Samples out of Range. . . Enter a number between 1 and the file length [{}]'.format(len(lines)))
         if args.number_of_waveforms[0] <= len(lines):
             numosamps = args.number_of_waveforms[0]
 
-    energy = []			#ENERGY ARRAY
+    energy = [] #ENERGY ARRAY
     energy_short = []
     waveforms = []
 
@@ -99,46 +75,35 @@ if args.psd_cut is not None:        #temporary check to prevent running errors w
     lb = 0
     #######"Loading Bar"###################
 
-    if len(args.scintillator) == 1:						
+    if len(args.scintillator) == 1:
         pmtloc = args.pmt[0]
         scintloc = args.scintillator[0]
-        vfilename = args.source[0] + '_data_run_' + str(args.run_number[0]) + "_" + args.scintillator[0]		#File names
+        vfilename = args.source[0] + '_data_run_' + str(args.run_number[0]) + "_" + args.scintillator[0] #File names
 
 
 
     for line in lines:
-        ener = int(line.split(";")[1])		
+        ener = int(line.split(";")[1])
         energy.append(ener)
-        samples = [int(s) for s in line.split(";")[4:]]				#Data samples for the set
+        samples = [int(s) for s in line.split(";")[4:]] #Data samples for the set
         ener_short = int(line.split(';')[2])
         energy_short.append(ener_short)
 
         voltage = np.array(samples) * 2.0 / (2**14 -1)  #Conversion from 2V range using 14 bit digitizer
-        time = 4e-9 * np.arange(voltage.size)           #Times of each voltage sample are seperated by 4 nanoseconds		#MOVE?#
+        time = 4e-9 * np.arange(voltage.size)           #Times of each voltage sample are seperated by 4 nanoseconds #MOVE?#
 
         #baseline = voltage[:nbase].sum() / nbase
         #voltage -= baseline
         waveforms.append(voltage)
 
-    eMode = mode(energy)				#Finds most common (highest) energy peak to scale against the most common source peaks
+    eMode = mode(energy) #Finds most common (highest) energy peak to scale against the most common source peaks
 
     psd = np.zeros(len(lines), np.float64)
-    #print(len(energy))
-    #print(len(psd))
-    #print(len(time))
-    #print(len(voltage))
-    #print(waveforms)
 
+    for e in range(len(energy)): #Scales energy and energy short to preset mode peak of source
 
-    for e in range(len(energy)):				#Scales energy and energy short to preset mode peak of source
-        #	energy[e] = float(energy[e] * (Scale_Marker / eMode))			#Energy long gate of set
-        #	energy_short[e] = float(energy_short[e] * (Scale_Marker / eMode))		#Energy short gate of set
-
-        if energy[e] != 0:							#Calculates PSD from energy long and short 
+        if energy[e] != 0: #Calculates PSD from energy long and short 
             psd[e] = float(energy[e] - energy_short[e]) / float(energy[e])
-            #print(psd)
-            #if energy[e] == 0:							#Prevents divide by zero error (have only seen it once)
-            #    psd[e] = 0.0
 
     energy = np.array(energy)
     waveforms = np.array(waveforms)
@@ -150,7 +115,7 @@ if args.psd_cut is not None:        #temporary check to prevent running errors w
     ##########################
     #####ENERGY HIST HERE#####
     plt.subplot(1,2,1)
-    plt.hist(energy, bins=400, range=[0,400], color='black')		#4096
+    plt.hist(energy, bins=400, range=[0,400], color='black')#4096
     plt.xlim(0,400)
     plt.subplot(1,2,2)
     plt.scatter(energy, psd, color='black')
@@ -164,7 +129,7 @@ if args.psd_cut is not None:        #temporary check to prevent running errors w
     #for l in range(len(psd)):
     
 
-    mask = (psd < args.psd_cut[1]) & (psd > args.psd_cut[0]) & (energy < args.energy_target[1]) & (energy > args.energy_target[0])							#Both psd and energy cuts for sorting data
+    mask = (psd < args.psd_cut[1]) & (psd > args.psd_cut[0]) & (energy < args.energy_target[1]) & (energy > args.energy_target[0]) #Both psd and energy cuts for sorting data
     masked_psd = psd[mask]
     masked_energy = energy[mask]
 
@@ -191,8 +156,8 @@ if args.psd_cut is not None:        #temporary check to prevent running errors w
 
         #print(waveform,waveform.shape)
 
-        time = np.insert(time, 0, masked_psd[j], axis=0)			#Inserts psd into array for saving to 1 file
-        waveform = np.insert(waveform, 0, masked_energy[j], axis=0)		#Inserts energy into array for saving to 1 file	
+        time = np.insert(time, 0, masked_psd[j], axis=0) #Inserts psd into array for saving to 1 file
+        waveform = np.insert(waveform, 0, masked_energy[j], axis=0) #Inserts energy into array for saving to 1 file 
         #print("energy is: ",energy[j])
         #print(waveform,waveform.shape)
 
